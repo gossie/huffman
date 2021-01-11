3b7bbc553888f4d5ca6f25764f8ebd8390f767c1
André Schreck <andre.schreck@gmail.com>
2021-01-11 09:44:25 +0100

    use bitwise operator

--- bitset.go
+++ bitset.go
@@ -1,7 +1,5 @@
 package huffman
 
-import "math"
-
 type bitset struct {
 	data         []uint64
 	lastSetIndex uint
@@ -16,7 +14,7 @@ func (b *bitset) isSet(index uint) bool {
 	if dataIndex >= uint(len(b.data)) {
 		return false
 	}
-	value := math.Pow(2, float64(index%64))
+	value := 1 << (index % 64)
 	return (b.data[dataIndex] & uint64(value)) > 0
 }
 
@@ -25,7 +23,7 @@ func (b *bitset) set(index uint) {
 	for i := uint(len(b.data)); i <= dataIndex; i++ {
 		b.data = append(b.data, 0)
 	}
-	value := math.Pow(2, float64(index%64))
+	value := 1 << (index % 64)
 	b.data[dataIndex] |= uint64(value)
 }
 
